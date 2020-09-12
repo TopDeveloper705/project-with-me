995ef190462db414854a489a49896c3c0b44bcc2
TopDeveloper705 <TopDeveloper705@outlook.com>
2020-09-12 13:22:47 +0200

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { TabsPageModule } from './tabs/tabs.module';
+import { WalkthroughPageModule } from 'src/pages/walkthrough/walkthrough.module';
+import { LoginPageModule } from 'src/pages/login/login.module';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'walkthrough',
+    loadChildren: () => WalkthroughPageModule,
+  },
+  {
+    path: 'login',
+    loadChildren: () => LoginPageModule,
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
