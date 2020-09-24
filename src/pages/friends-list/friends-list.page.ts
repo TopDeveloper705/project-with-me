2766aa0c75f0122b8689e53545ee1f7729663040
TopDeveloper705 <TopDeveloper705@outlook.com>
2020-09-24 18:21:01 +0200

    Added Friends

--- src/pages/friends-list/friends-list.page.ts
+++ src/pages/friends-list/friends-list.page.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friends-list.page.scss'],
 })
 export class FriendsListPage implements OnInit {
+  friends = [
+    { name: 'Mathis Monn' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+    { name: 'Daniel Ehrhardt' },
+  ];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
   }
-
 }
