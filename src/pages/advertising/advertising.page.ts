384eec7ff103cc71a6fd7a27673bded7faa63a8f
TopDeveloper705 <TopDeveloper705@outlook.com>
2020-12-05 15:22:09 +0100

    Added Ads Page

--- src/pages/advertising/advertising.page.ts
+++ src/pages/advertising/advertising.page.ts
@@ -1,3 +1,4 @@
+import { HelperService } from './../../common/services/helper.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,15 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdvertisingPage implements OnInit {
   slides = [
-    { title: 'Bar 1' },
-    { title: 'Bar 2' },
-    { title: 'Bar 3' },
-    { title: 'Bar 4' },
-    { title: 'Bar 5' },
-    { title: 'Bar 6' },
-    { title: 'Bar 7' },
+    { title: 'shishaworldsamdanli', image: '/assets/images/stories/1.jpg' },
+    { title: 'shisha_station_official', image: '/assets/images/stories/2.jpg' },
+    { title: 'shisharia', image: '/assets/images/stories/3.jpg' },
+    { title: 'shishazentrale', image: '/assets/images/stories/4.jpg' },
   ];
-  constructor() {}
+  constructor(public helper: HelperService) {}
 
   ngOnInit() {}
 }
