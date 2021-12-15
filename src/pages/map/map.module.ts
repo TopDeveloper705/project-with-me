428d850598f5e6976de21d4b68ce0c2c0cbbdbc7
TopDeveloper705 <TopDeveloper705@outlook.com>
2021-12-15 13:53:24 +0100

    Friend List

--- src/pages/map/map.module.ts
+++ src/pages/map/map.module.ts
@@ -7,9 +7,11 @@ import { MapFilterComponent } from "./components/map-filter/map-filter.component
 import { MapPageRoutingModule } from "./map-routing.module";
 import { MapPage } from "./map.page";
 import { ReactiveFormsModule } from "@angular/forms";
+import { FriendsListPageModule } from "../friends-list/friends-list.module";
+import { FriendsListPage } from "../friends-list/friends-list.page";
 @NgModule({
   imports: [GoogleMapsModule, CommonModule, FormsModule, IonicModule, MapPageRoutingModule, ReactiveFormsModule],
-  declarations: [MapPage, MapFilterComponent],
+  declarations: [MapPage, MapFilterComponent, FriendsListPage],
   exports: [MapPage],
 })
 export class MapPageModule {}
