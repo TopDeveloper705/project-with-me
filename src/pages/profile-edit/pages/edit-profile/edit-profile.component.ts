ba8d2a33398c24379b8cd071e3f7522a3c553f9c
TopDeveloper705 <TopDeveloper705@outlook.com>
2021-12-18 17:43:09 +0100

    Added Profile Edit Page

--- src/pages/profile-edit/pages/edit-profile/edit-profile.component.ts
+++ src/pages/profile-edit/pages/edit-profile/edit-profile.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
 
 import { Geolocation } from '@capacitor/geolocation';
 
-import { LoadingController, ModalController } from '@ionic/angular';
+import { LoadingController, ModalController, ToastController } from '@ionic/angular';
 import { AuthService } from 'src/common/auth/_services/auth.service';
 import { MapHelperService } from 'src/common/services/map-helper.service';
 
@@ -13,9 +13,21 @@ import { MapHelperService } from 'src/common/services/map-helper.service';
   styleUrls: ['./edit-profile.component.scss'],
 })
 export class EditProfileComponent implements OnInit {
-  locationLoading = false;
-  saveLoading = false;
-  options: any = {};
+
+  currentUser: any;
+  public model: {
+    username: string;
+    password: string;
+    telegram: string;
+  } = {
+      username: '',
+      password: '',
+      telegram: ''
+    }
+
+  user: any
+
+
 
   constructor(
     private cdr: ChangeDetectorRef,
@@ -23,101 +35,58 @@ export class EditProfileComponent implements OnInit {
     public mapHelperService: MapHelperService,
     private modalCtrl: ModalController,
     private http: HttpClient,
+    private toastCtrl: ToastController,
     private authService: AuthService
   ) { }
 
-  async ngOnInit() { }
-
-  async getCurrentPosition() {
-    this.locationLoading = true;
-    try {
-      const coordinates = await Geolocation.getCurrentPosition();
-      console.log('coordinates', coordinates);
-      this.options.center = {
-        lat: coordinates.coords.latitude,
-        lng: coordinates.coords.longitude,
-      };
-
-      await this.findLocationT(coordinates.coords);
-    } catch (error) {
-      console.log('error', error);
-    } finally {
-      this.locationLoading = false;
-    }
+  async ngOnInit() {
+    await this.loadUser()
+    this.model.username = this.user.customUsername
+    this.model.telegram = this.user.telegramUsername
   }
 
-  findLocationT(coords) {
-    const latlng = {
-      lat: parseFloat(coords.latitude),
-      lng: parseFloat(coords.longitude),
-    };
-    const geocoder = new google.maps.Geocoder();
-    geocoder.geocode({ location: latlng }, (response: any) => {
-      console.log('response', response);
-      if (response[0]) {
-        console.log(response[0]);
-        const center = new google.maps.LatLng(
-          coords.latitude,
-          coords.longitude
-        );
+  async loadUser() {
+    const data = await this.http
+      .get('api/users/' + this.authService?.user.id)
+      .toPromise();
+    this.user = data;
+    console.log('this.user', this.user);
 
-        this.mapHelperService.postalCode = '';
-        this.mapHelperService.location = response[0];
-        this.mapHelperService.postalCode = this.mapHelperService.getField(
-          response[0].address_components,
-          'postal_code'
-        ).short_name;
-
-        this.cdr.detectChanges();
-      } else {
-        window.alert('No results found');
-      }
-    });
-  }
-
-  async findLocation() {
-    const loading = await this.loadingCtrl.create({ mode: 'md' });
-    loading.present();
-
-    const request = {
-      query: this.mapHelperService.postalCode,
-      fields: ['ALL'],
-    };
-
-    const service = new google.maps.places.PlacesService(
-      document.createElement('div')
-    );
-
-    service.findPlaceFromQuery(request, (results, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        console.log(results);
-        if (results[0]) {
-          this.mapHelperService.postalCode = '';
-          console.log(results[0]);
-          this.mapHelperService.location = results[0];
-          this.cdr.detectChanges();
-        }
-      }
-      loading.dismiss();
-    });
-  }
-
-  close() {
-    this.modalCtrl.dismiss();
+    await this.authService.updateUser();
+    this.cdr.detectChanges();
   }
 
   async save() {
-    this.saveLoading = true;
-    const location = {
-      address: this.mapHelperService.location.formatted_address,
-      lat: this.options.center.lat,
-      lng: this.options.center.lng,
-    };
-    await this.http
-      .put('api/users/' + this.authService.user.id, { location: location })
-      .toPromise();
-    await this.mapHelperService.save();
-    this.saveLoading = false;
-    this.modalCtrl.dismiss();
+    const update = {} as any
+
+    if (this.model.password) update.password = this.model.password
+    if (this.model.telegram != this.user.telegramUsername) update.telegramUsername = this.model.telegram
+    if (this.model.username != this.user.customUsername) update.customUsername = this.model.username
+
+
+    try {
+      const user: any = await this.http.put('api/users/' + this.user.id, update).toPromise();
+      await this.loadUser();
+      (
+        await this.toastCtrl.create({
+          message: 'Änderungen gespeichert',
+          translucent: true,
+          position: 'top',
+          duration: 4000
+        })
+      ).present()
+    } catch (error) {
+      (
+        await this.toastCtrl.create({
+          message: error,
+          translucent: true,
+          position: 'top',
+          duration: 4000
+        })
+      ).present()
+
+    }
+
   }
+
 }
