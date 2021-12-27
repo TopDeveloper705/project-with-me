5ac5bc540aba40c3b1c40b70972da4b24272a4ca
TopDeveloper705 <TopDeveloper705@outlook.com>
2021-12-27 23:25:07 +0100

    Show nearby Shisha Bars before the search dialog shows up

--- src/pages/dashboard/components/start-session-modal/start-session-modal.component.ts
+++ src/pages/dashboard/components/start-session-modal/start-session-modal.component.ts
@@ -9,11 +9,23 @@ import { MapService } from 'src/common/services/map.service';
 import { Manufacturer } from 'src/common/types';
 import { SelectLocationPage } from 'src/pages/select-location/select-location.page';
 import { slideOpts } from '../../slider-config';
+import { trigger, transition, animate, style } from '@angular/animations'
 
 @Component({
   selector: 'app-start-session-modal',
   templateUrl: './start-session-modal.component.html',
   styleUrls: ['./start-session-modal.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)' }),
+        animate('400ms ease-in', style({ transform: 'translateY(0%)' }))
+      ]),
+      transition(':leave', [
+        animate('400ms ease-in', style({ transform: 'translateY(100%)' }))
+      ])
+    ])
+  ]
 })
 export class StartSessionModalComponent implements OnInit {
   @Input() product: Manufacturer;
@@ -27,6 +39,8 @@ export class StartSessionModalComponent implements OnInit {
   slideOpts = slideOpts;
   locationLoading: boolean = false;
   visiblityState = 'hidden';
+  selectLocationMode = false;
+  nearbyShishaBars = [];
 
   constructor(
     public mapService: MapService,
@@ -39,7 +53,7 @@ export class StartSessionModalComponent implements OnInit {
     private toastCtrl: ToastController,
     public platform: Platform,
     private router: Router,
-    private route: ActivatedRoute, 
+    private route: ActivatedRoute,
     private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
@@ -47,7 +61,48 @@ export class StartSessionModalComponent implements OnInit {
     console.log('B', this.type);
   }
 
-  async selectLocation() {
+  private calculateDistance(lat1, lon1, lat2, lon2, unit) {
+    console.log(lat1, lon1, lat2, lon2, unit);
+    const radlat1 = (Math.PI * lat1) / 180;
+    const radlat2 = (Math.PI * lat2) / 180;
+    const theta = lon1 - lon2;
+    const radtheta = (Math.PI * theta) / 180;
+    let dist =
+      Math.sin(radlat1) * Math.sin(radlat2) +
+      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist);
+    dist = (dist * 180) / Math.PI;
+    dist = dist * 60 * 1.1515;
+    if (unit == 'K') {
+      dist = dist * 1.609344;
+    }
+    if (unit == 'N') {
+      dist = dist * 0.8684;
+    }
+    return dist;
+  }
+
+  public async selectLocation(): Promise<void> {
+    if (this.authService.user.location) {
+      const locations: any = await this.http
+        .get('api/locations')
+        .toPromise();
+
+      locations.map((location) => {
+        location.distance = this.calculateDistance(location.location.lat, location.location.lng, this.authService.user.location.lat, this.authService.user.location.lng, null);
+      })
+
+      const sorted = locations.sort((a, b) => a.distance > b.distance ? 1 : -1);
+      this.nearbyShishaBars = [sorted[0], sorted[1], sorted[2]];
+      this.selectLocationMode = true;
+    } else this.openSearchLocationModal();
+  }
+
+  async locationClicked(location) {
+    await this.startSession(this.product, this.type, location);
+  }
+
+  async openSearchLocationModal() {
     const elm = await this.modalCtrl.getTop();
     const modal = await this.modalCtrl.create({
       component: SelectLocationPage,
@@ -180,7 +235,7 @@ export class StartSessionModalComponent implements OnInit {
       }
 
     } catch (error) {
-      
+
     } finally {
       loading.dismiss();
     }
